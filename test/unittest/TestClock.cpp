146f73638a43dee55f6c4f71a548313fd37f2bfc
mrxu <996100533@qq.com>
2024-08-03 18:05:18 +0800

    add unit test for clock ram and rom

---
+++ test/unittest/TestClock.cpp
@@ -0,0 +1,92 @@
+
+#include <gtest/gtest.h>
+
+#include "Clock.h"
+
+using namespace tones;
+
+class Counter : public Tickable
+{
+
+public:
+
+    int value = 0;
+
+    Counter(int multiplier=1) : Tickable(multiplier) {}
+
+    void _tick() { ++value; }
+
+    int operator()() { return value; }
+};
+
+TEST(TestClock, TickTest)
+{
+    const int num = 10;
+    const int mul = 5;
+
+    Clock clock;
+    Counter counter_1;
+    Counter counter_2(mul);
+
+    counter_1.attach(clock);
+    counter_2.attach(clock);
+    for (int i = 1; i <= num; ++i) {
+        clock.tick();
+        EXPECT_EQ(counter_1(), i);
+        EXPECT_EQ(counter_2(), mul * i);
+    }
+}
+
+TEST(TestClock, AttatchTest)
+{
+    const int num = 10;
+
+    Clock clock;
+    Counter counter_1(1);
+    Counter counter_2(1);
+
+    counter_1.attach(clock);
+    for (int i = 1; i <= num; ++i)
+        clock.tick();
+    EXPECT_EQ(counter_1(), num);
+
+    counter_2.attach(clock);
+    for (int i = 1; i <= num; ++i)
+        clock.tick();
+    EXPECT_EQ(counter_1(), 2 * num);
+    EXPECT_EQ(counter_2(), num);
+}
+
+TEST(TestClock, DetatchTest)
+{
+    const int num = 10;
+
+    Clock clock;
+    Counter counter_1(1);
+    Counter counter_2(1);
+    Counter counter_3(1);
+
+    counter_1.attach(clock);
+    counter_2.attach(clock);
+    counter_3.attach(clock);
+    for (int i = 1; i <= num; ++i)
+        clock.tick();
+
+    counter_3.detach();
+    for (int i = 1; i <= num; ++i)
+        clock.tick();
+
+    counter_2.detach();
+    for (int i = 1; i <= num; ++i)
+        clock.tick();
+
+    EXPECT_EQ(counter_1(), 3 * num);
+    EXPECT_EQ(counter_2(), 2 * num);
+    EXPECT_EQ(counter_3(), num);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ test/unittest/TestMemory.cpp
@@ -0,0 +1,136 @@
+
+#include <stdlib.h>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "Device.h"
+
+using namespace tones;
+
+const int RomSize = ReadOnlyMemory::RomUpperBankBase - ReadOnlyMemory::RomLowerBankBase;
+
+class TestMemory : public ::testing::Test
+{
+
+protected:
+
+    TestMemory() : _data(RomSize, 0), _rom(_data) {}
+
+    void SetUp() override
+    {
+        _ram.attach(_bus);
+        _rom.attach(_bus);
+    }
+
+    void TearDown() override
+    {
+        _ram.detach();
+        _rom.detach();
+    }
+
+    Bus _bus;
+    RandomAccessMemory _ram;
+    ReadOnlyMemory _rom;
+    std::vector<uint8_t> _data;
+};
+
+TEST_F(TestMemory, RamReadWrite)
+{
+    uint16_t addr;
+    uint8_t buffer, data;
+
+    // Sequencely
+    for (uint16_t i = 0; i < RandomAccessMemory::RamSize; ++i) {
+        data = rand() & 0xff;
+        _bus.write(i, data);
+        _bus.read(i, buffer);
+        EXPECT_EQ(buffer, data);
+    }
+
+    // Randomly
+    for (uint16_t i = 0; i < RandomAccessMemory::RamSize; ++i) {
+        data = i & 0xff;
+        addr = rand() & RandomAccessMemory::RamMask;
+        _bus.write(addr, data);
+        _bus.read(addr, buffer);
+        EXPECT_EQ(buffer, data);
+    }
+}
+
+TEST_F(TestMemory, RamMirror)
+{
+    uint16_t addr;
+    uint8_t buffer, data;
+
+    EXPECT_EQ(true, _ram.contains(RandomAccessMemory::RamLowerBound));
+    EXPECT_EQ(true, _ram.contains(RandomAccessMemory::RamUpperBound - 1));
+    EXPECT_EQ(false, _ram.contains(RandomAccessMemory::RamUpperBound));
+
+    // Sequencely
+    for (uint16_t i = RandomAccessMemory::RamSize; i < RandomAccessMemory::RamUpperBound; ++i) {
+        data = rand() & 0xff;
+        _bus.write(i, data);
+
+        addr = i & RandomAccessMemory::RamMask;
+
+        _bus.read(addr, buffer);
+        EXPECT_EQ(buffer, data);
+
+        _bus.read(addr + RandomAccessMemory::RamSize, buffer);
+        EXPECT_EQ(buffer, data);
+
+        _bus.read(addr + 2 * RandomAccessMemory::RamSize, buffer);
+        EXPECT_EQ(buffer, data);
+
+        _bus.read(addr + 3 * RandomAccessMemory::RamSize, buffer);
+        EXPECT_EQ(buffer, data);
+    }
+
+    // Randomly
+    for (uint16_t i = RandomAccessMemory::RamSize; i < RandomAccessMemory::RamUpperBound; ++i) {
+        data = i & 0xff;
+        addr = rand() % RandomAccessMemory::RamUpperBound;
+        _bus.write(addr, data);
+
+        addr &= RandomAccessMemory::RamMask;
+
+        _bus.read(addr, buffer);
+        EXPECT_EQ(buffer, data);
+
+        _bus.read(addr + RandomAccessMemory::RamSize, buffer);
+        EXPECT_EQ(buffer, data);
+
+        _bus.read(addr + 2 * RandomAccessMemory::RamSize, buffer);
+        EXPECT_EQ(buffer, data);
+
+        _bus.read(addr + 3 * RandomAccessMemory::RamSize, buffer);
+        EXPECT_EQ(buffer, data);
+    }
+}
+
+TEST_F(TestMemory, RomReadWrite)
+{
+    uint16_t addr;
+    uint8_t buffer, data;
+
+    for (int i = 0; i < RomSize; ++i)
+        _data[i] = i & 0xff;
+    
+    // Sequencely
+    for (uint16_t i = 0; i < RomSize; ++i) {
+        addr = (ReadOnlyMemory::RomLowerBankBase + i) & 0xffff;
+
+        data = i & 0xff;
+        _bus.write(addr, data + 1);
+
+        _bus.read(addr, buffer);
+        EXPECT_EQ(buffer, data);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
